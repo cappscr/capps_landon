348ff665cc314b727bfe01acd7350b0181845a82
cappscr <capps.christopher@gmail.com>
2024-03-08 17:02:02 -0500

    Dynamic data for header and welcome images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import menuLinksData from './data/menuLinks.json'
+
 export const Header = () => {
   return (
     <header id="intro">
@@ -5,22 +8,35 @@ export const Header = () => {
         <div className="hgroup">
           <h1>Landon Hotel</h1>
           <h2>West London</h2>
-          <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+          <p>
+            <a href="#welcome">
+              <img
+                src="https://landonhotel.com/images/misc/arrow.png"
+                alt="down arrow"
+              />
+            </a>
+          </p>
         </div>
       </article>
 
       <nav id="nav">
         <div className="navbar">
-          <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+          <div className="brand">
+            <a href="#welcome">
+              Landon <span>Hotel</span>
+            </a>
+          </div>
           <ul>
-            <li><a className="icon info" href="#hotelinfo"><span>info</span></a></li>
-            <li><a className="icon rooms" href="#rooms"><span>rooms</span></a></li>
-            <li><a className="icon dining" href="#dining"><span>dining</span></a></li>
-            <li><a className="icon events" href="#events"><span>events</span></a></li>
-            <li><a className="icon attractions" href="#attractions"><span>attractions</span></a></li>
+            {menuLinksData.map((link) => (
+              <li key={link.text}>
+                <a className={`icon ${link.class}`} href={link.href}>
+                  <span>{link.text}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
     </header>
-  );
+  )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,37 @@
+import welcomeImagesData from './data/welcomeImages.json'
+
 export const Welcome = () => {
   return (
     <div className="scene" id="welcome">
       <article className="content">
         <div className="gallery">
-          <img src="https://landonhotel.com/images/hotel/intro_room.jpg" alt="Intro Gallery Room Sample Pictures" />
-          <img src="https://landonhotel.com/images/hotel/intro_pool.jpg" alt="Intro Gallery Pool Sample Pictures" />
-          <img src="https://landonhotel.com/images/hotel/intro_dining.jpg" alt="Intro Gallery Dining Sample Pictures" />
-          <img src="https://landonhotel.com/images/hotel/intro_attractions.jpg" alt="Intro Gallery Attractions Sample Pictures" />
-          <img className="hidesm" src="https://landonhotel.com/images/hotel/intro_wedding.jpg" alt="Intro Gallery Dining Sample Pictures" />
+          {welcomeImagesData.map((image) => {
+            if (image.class)
+              return (
+                <img
+                  key={image.src}
+                  className={`${image.class}`}
+                  src={image.src}
+                  alt={image.alt}
+                />
+              )
+            return <img key={image.src} src={image.src} alt={image.alt} />
+          })}
         </div>
         <h1>Welcome to the Landon&nbsp;Hotel</h1>
-        <p>The original Landon perseveres after 50 years in the heart of West London. The West End neighborhood has something for everyone—from theater to dining to historic sights. And the not-to-miss Rooftop Cafe is a great place for travelers and locals to engage over drinks, food, and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel in the West End, browse our website and <a href="files/landon_information_sheet_London.pdf">download our handy information sheet</a>.</p>
+        <p>
+          The original Landon perseveres after 50 years in the heart of West
+          London. The West End neighborhood has something for everyone—from
+          theater to dining to historic sights. And the not-to-miss Rooftop Cafe
+          is a great place for travelers and locals to engage over drinks, food,
+          and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel
+          in the West End, browse our website and{' '}
+          <a href="files/landon_information_sheet_London.pdf">
+            download our handy information sheet
+          </a>
+          .
+        </p>
       </article>
     </div>
-  );
+  )
 }
